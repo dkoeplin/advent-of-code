1d110a5ae05fdc08067e7bea2e1eff86fac89387
David Alan Koeplinger <david.koeplinger@sambanovasystems.com>
2021-12-02 19:28:38 -0500

    Initial commit

--- src/Main.scala
+++ src/Main.scala
@@ -1,4 +1,9 @@
 
 object Main extends App {
-  println("Hello")
+  val file = scala.io.Source.fromFile("./data/1.1")
+  val increased = file.getLines().map(_.trim().toInt).sliding(2).map{case Seq(prev, current) =>
+    if (current > prev) 1 else 0
+  }.sum
+  file.close()
+  println(increased)
 }
