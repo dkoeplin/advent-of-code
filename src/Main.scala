57f7b9a7b022c26d0a6076b7191b8da89030c6f6
David Alan Koeplinger <david.koeplinger@sambanovasystems.com>
2021-12-02 20:02:43 -0500

    Initial commit

--- src/Main.scala
+++ src/Main.scala
@@ -1,9 +1,17 @@
 
+case class Position(horizontal: Int = 0, vertical: Int = 0) {
+  def up(dist: Int): Position = Position(horizontal, vertical + dist)
+  def move(dist: Int): Position = Position(horizontal + dist, vertical)
+}
+
 object Main extends App {
-  val file = scala.io.Source.fromFile("./data/1.1")
-  val increased = file.getLines().map(_.trim().toInt).sliding(2).map{case Seq(prev, current) =>
-    if (current > prev) 1 else 0
-  }.sum
+  val file = scala.io.Source.fromFile("./data/2")
+
+  val position = file.getLines().foldLeft(Position()){
+    case (pos, cmd) if cmd.startsWith("forward") => pos.move(cmd.drop(8).toInt)
+    case (pos, cmd) if cmd.startsWith("down") => pos.up(cmd.drop(5).toInt)
+    case (pos, cmd) if cmd.startsWith("up") => pos.up(-cmd.drop(3).toInt)
+  }
   file.close()
-  println(increased)
+  println(position.horizontal * position.vertical)
 }
