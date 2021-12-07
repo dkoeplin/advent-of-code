52cfa15193c8eb1f5c9694dd68ec7ed178655df1
David Alan Koeplinger <david.koeplinger@sambanovasystems.com>
2021-12-06 21:11:34 -0500

    Commit!

--- src/Main.scala
+++ src/Main.scala
@@ -1,26 +1,24 @@
 
-
-case class BitCount(ones: Array[Int] = Array.empty, total: Int = 0) {
-  def +(str: String): BitCount = {
-    val rhs = BitCount(str)
-    if (ones.isEmpty) BitCount(rhs.ones, 1) else BitCount(ones.zip(rhs.ones).map{case (a,b) => a + b}, total + 1)
-  }
-  def bits(): String = ones.map{count => if (count >= total / 2) 1 else 0 }.mkString("")
+case class BitCount(counts: Seq[Int] = Array.fill(32)(0), total: Int = 0) {
+  def bits(): String =
   def flippedBits(): String = bits().map{c => if (c == '1') '0' else '1'}
   def toGamma: Int = Integer.parseInt(bits(), 2)
   def toAlpha: Int = Integer.parseInt(flippedBits(), 2)
-  println(s"${ones.mkString(",")}; total: $total")
-}
-
-object BitCount {
-  def apply(str: String) = new BitCount(str.trim().map{c => if (c == '1') 1 else 0}.toArray)
 }
 
 object Main extends App {
   val file = scala.io.Source.fromFile("./data/3")
 
-  val counts = file.getLines().foldLeft(BitCount()){(gamma, line) => gamma + line }
-  val gamma = counts.toGamma
+  var length = 0
+  var total = 0
+  val counts = file.getLines().zipWithIndex.map{case (line, i) =>
+    Integer.parseInt(_,2) 
+  }
+    .foldLeft(Seq.fill(32)(0)){(accum, value: Int) =>
+      total += 1
+      (31 to 0 by -1).map{i => accum(31 - i) + ((value & 0x1 << i) >> i) }
+  }
+  val gamma = counts.map{count => if (2 * count >= total) 1 else 0 }.mkString("")
   val alpha = counts.toAlpha
   file.close()
 
