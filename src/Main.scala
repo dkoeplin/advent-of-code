feecdecc5335dd3533a1903585ab5546f486d2ad
David Alan Koeplinger <david.koeplinger@sambanovasystems.com>
2021-12-07 00:19:00 -0500

    Commit!

--- src/Main.scala
+++ src/Main.scala
@@ -1,50 +1,22 @@
-import scala.collection.mutable
-import scala.util.Try
+case class Pos(x: Int, y: Int)
 
-case class Pos(row: Int, col: Int)
-
-case class Board(id: Int, board: Seq[Seq[Int]]) {
-  val numRows: Int = board.size
-  val numCols: Int = board.head.size
-  var score: Int = board.map(_.sum).sum
-  val rows: mutable.Seq[Int] = mutable.Seq.fill(numCols)(0)
-  val cols: mutable.Seq[Int] = mutable.Seq.fill(numRows)(0)
-
-  def posOf(x: Int): Option[Pos] = board.zipWithIndex.flatMap{case (row,idx) =>
-    Some(Pos(idx, row.indexOf(x))).filter(_.col != -1)
-  }.headOption
-
-  def register(x: Int): Boolean = posOf(x).exists{pos =>
-    rows(pos.row) += 1
-    cols(pos.col) += 1
-    score = score - x
-    println(s"Board $id: $x at (${pos.row}, ${pos.col}) (row: ${rows(pos.row)} / $numRows, col: ${cols(pos.col)} / $numCols)")
-    rows(pos.row) == numRows || cols(pos.col) == numCols
+case class Line(x1: Int, y1: Int, x2: Int, y2: Int) {
+  def points: Seq[Pos] = {
+    val length = Math.max(Math.abs(x2 - x1), Math.abs(y2 - y1))
+    val x_step = if (x1 > x2) -1 else if (x1 < x2) 1 else 0
+    val y_step = if (y1 > y2) -1 else if (y1 < y2) 1 else 0
+    (0 to length).map{i => Pos(x1 + x_step*i, y1 + y_step*i) }
   }
 }
 
 object Main extends App {
-  val file = scala.io.Source.fromFile("./data/4")
-  val lines = file.getLines()
+  val Pattern = "([0-9]+),([0-9]+) -> ([0-9]+),([0-9]+)".r
+  val file = scala.io.Source.fromFile("./data/5")
+  val repeats = file.getLines().map{case Pattern(x1,y1,x2,y2) => Line(x1.toInt,y1.toInt,x2.toInt,y2.toInt) }
+    .foldLeft((Set.empty[Pos], Set.empty[Pos])){(previous, line) =>
+      val points = line.points.toSet
+      (previous._1 ++ points, previous._2 ++ (previous._1 intersect points))
+    }._2.size
 
-  val numbers = lines.next().split(",").flatMap(str => if (str.isEmpty) None else Some(str.toInt))
-  var boards: Set[Board] = Set.empty
-  while (lines.hasNext) {
-    val entries: Seq[Seq[Int]] = lines.takeWhile(_.nonEmpty).map(_.split("\\s+").flatMap{ x => Try {x.toInt }.toOption }.toSeq).toSeq
-    if (entries.nonEmpty)
-      boards += Board(boards.size, entries)
-  }
-
-  var iter = numbers.iterator
-  var called: Int = 0
-  var last: Option[Board] = None
-  while (iter.hasNext && boards.nonEmpty) {
-    called = iter.next()
-    println(s"Called $called")
-    boards = boards.filterNot(_.register(called))
-    if (last.isEmpty && boards.size == 1)
-      last = boards.headOption
-  }
-  val score = last.get.score
-  println(s"called: $called, score: $score, final: ${score * called}")
+  println(s"repeats: $repeats")
 }
