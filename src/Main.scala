2365e93a72a942dc6713b577008705c8f94afde8
David Alan Koeplinger <david.koeplinger@sambanovasystems.com>
2021-12-06 22:56:43 -0500

    Commit!

--- src/Main.scala
+++ src/Main.scala
@@ -1,29 +1,50 @@
+import scala.collection.mutable
+import scala.util.Try
 
-case class BitCount(counts: Seq[Int] = Array.fill(32)(0), total: Int = 0) {
-  def bits(): String =
-  def flippedBits(): String = bits().map{c => if (c == '1') '0' else '1'}
-  def toGamma: Int = Integer.parseInt(bits(), 2)
-  def toAlpha: Int = Integer.parseInt(flippedBits(), 2)
+case class Pos(row: Int, col: Int)
+
+case class Board(board: Seq[Seq[Int]]) {
+  val numRows: Int = board.size
+  val numCols: Int = board.head.size
+  var score: Int = board.map(_.sum).sum
+  val rows: mutable.Seq[Int] = mutable.Seq.fill(numCols)(0)
+  val cols: mutable.Seq[Int] = mutable.Seq.fill(numRows)(0)
+
+  def posOf(x: Int): Option[Pos] = board.zipWithIndex.flatMap{case (row,idx) =>
+    Some(Pos(idx, row.indexOf(x))).filter(_.col != -1)
+  }.headOption
+
+  def register(x: Int): Boolean = posOf(x).exists{pos =>
+    rows(pos.row) += 1
+    cols(pos.col) += 1
+    score = score - x
+    println(s"Board $x at (${pos.row}, ${pos.col}) (row: ${rows(pos.row)} / $numRows, col: ${cols(pos.col)} / $numCols)")
+    rows(pos.row) == numRows || cols(pos.col) == numCols
+  }
 }
 
 object Main extends App {
-  val file = scala.io.Source.fromFile("./data/3")
+  val file = scala.io.Source.fromFile("./data/4")
+  val lines = file.getLines()
 
-  var length = 0
-  var total = 0
-  val counts = file.getLines().zipWithIndex.map{case (line, i) =>
-    Integer.parseInt(_,2) 
+  val numbers = lines.next().split(",").flatMap(str => if (str.isEmpty) None else Some(str.toInt))
+  var boards: Seq[Board] = Seq.empty
+
+  while (lines.hasNext) {
+    val entries: Seq[Seq[Int]] = lines.takeWhile(_.nonEmpty).map(_.split("\\s+").flatMap{ x => Try {x.toInt }.toOption }.toSeq).toSeq
+    if (entries.nonEmpty)
+      boards = boards :+ Board(entries)
   }
-    .foldLeft(Seq.fill(32)(0)){(accum, value: Int) =>
-      total += 1
-      (31 to 0 by -1).map{i => accum(31 - i) + ((value & 0x1 << i) >> i) }
+
+  var iter = numbers.iterator
+  var called: Int = 0
+  var winner: Option[Board] = None
+  while (iter.hasNext && winner.isEmpty) {
+    called = iter.next()
+    println(s"Called $called")
+    winner = boards.find(_.register(called))
   }
-  val gamma = counts.map{count => if (2 * count >= total) 1 else 0 }.mkString("")
-  val alpha = counts.toAlpha
-  file.close()
-
-  println(s"${counts.bits()}: $gamma")
-  println(s"${counts.flippedBits()}: $alpha")
-
-  println(gamma * alpha)
+  assert(winner.nonEmpty)
+  val score = winner.get.score
+  println(s"called: $called, score: $score, final: ${score * called}")
 }
