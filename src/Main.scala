876fc916fb5405e249898a097ced12aade7168a1
David Alan Koeplinger <david.koeplinger@sambanovasystems.com>
2021-12-06 23:09:19 -0500

    Commit!

--- src/Main.scala
+++ src/Main.scala
@@ -3,7 +3,7 @@ import scala.util.Try
 
 case class Pos(row: Int, col: Int)
 
-case class Board(board: Seq[Seq[Int]]) {
+case class Board(id: Int, board: Seq[Seq[Int]]) {
   val numRows: Int = board.size
   val numCols: Int = board.head.size
   var score: Int = board.map(_.sum).sum
@@ -18,7 +18,7 @@ case class Board(board: Seq[Seq[Int]]) {
     rows(pos.row) += 1
     cols(pos.col) += 1
     score = score - x
-    println(s"Board $x at (${pos.row}, ${pos.col}) (row: ${rows(pos.row)} / $numRows, col: ${cols(pos.col)} / $numCols)")
+    println(s"Board $id: $x at (${pos.row}, ${pos.col}) (row: ${rows(pos.row)} / $numRows, col: ${cols(pos.col)} / $numCols)")
     rows(pos.row) == numRows || cols(pos.col) == numCols
   }
 }
@@ -28,23 +28,23 @@ object Main extends App {
   val lines = file.getLines()
 
   val numbers = lines.next().split(",").flatMap(str => if (str.isEmpty) None else Some(str.toInt))
-  var boards: Seq[Board] = Seq.empty
-
+  var boards: Set[Board] = Set.empty
   while (lines.hasNext) {
     val entries: Seq[Seq[Int]] = lines.takeWhile(_.nonEmpty).map(_.split("\\s+").flatMap{ x => Try {x.toInt }.toOption }.toSeq).toSeq
     if (entries.nonEmpty)
-      boards = boards :+ Board(entries)
+      boards += Board(boards.size, entries)
   }
 
   var iter = numbers.iterator
   var called: Int = 0
-  var winner: Option[Board] = None
-  while (iter.hasNext && winner.isEmpty) {
+  var last: Option[Board] = None
+  while (iter.hasNext && boards.nonEmpty) {
     called = iter.next()
     println(s"Called $called")
-    winner = boards.find(_.register(called))
+    boards = boards.filterNot(_.register(called))
+    if (last.isEmpty && boards.size == 1)
+      last = boards.headOption
   }
-  assert(winner.nonEmpty)
-  val score = winner.get.score
+  val score = last.get.score
   println(s"called: $called, score: $score, final: ${score * called}")
 }
