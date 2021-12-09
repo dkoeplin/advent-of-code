a308778f1e8144a3aee31bb2af8c46a86911bb20
David Alan Koeplinger <david.koeplinger@sambanovasystems.com>
2021-12-08 23:14:07 -0500

    Commit!

--- src/Main.scala
+++ src/Main.scala
@@ -1,23 +1,24 @@
 object Main extends App {
-  def fuelCost(dist: Long): Long = dist*(dist + 1)/2 // sum of 1...N
+  val nums = Seq("abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg")
+  val reverseDictionary: Map[String,Int] = nums.zipWithIndex.map{case (word,v) => word -> v }.toMap
 
-  val Num = "([0-9]+)".r
-  val file = scala.io.Source.fromFile("./data/7")
-  var minPos: Int = Int.MaxValue
-  var maxPos: Int = Int.MinValue
-  val crabs = file.getLines().next().split(",").flatMap{
-    case Num(x) =>
-      val pos = x.toInt
-      minPos = Math.min(minPos, pos)
-      maxPos = Math.max(maxPos, pos)
-      Some(pos)
-    case _ => None
-  }
-  val min = (minPos to maxPos).foldLeft((Long.MaxValue,Long.MaxValue)){(accum,pos) =>
-      val cost = crabs.foldLeft(0L){(accum, crab) => accum + fuelCost(Math.abs(crab - pos)) }
-      println(s"$pos: $cost")
-      if (cost < accum._2) (pos, cost) else accum
+  def signature(c: Char, words: Seq[String]): List[Int] = words.groupBy(_.length).map{case (len, ws) => len -> ws.map{_.count(_ == c) }.sum }.toList.sortBy(_._1).map(_._2)
+
+  val signatureMap: Map[List[Int],Char] = ('a' to 'g').map{c => signature(c, nums) -> c }.toMap
+
+  val file = scala.io.Source.fromFile("./data/8")
+  val count = file.getLines().foldLeft((0,0)){(accum, line) =>
+    val input = line.split(" \\| ")
+    val unique = input(0).split(" ")
+    val output = input(1).split(" ")
+    val mapping: Map[Char,Char] = ('a' to 'g').map{c => c -> signatureMap(signature(c, unique)) }.toMap
+
+    val decoded = output.map{encoded => reverseDictionary(encoded.map{c => mapping(c)}.sorted) }
+    val count = accum._1 + decoded.count{v => v == 1 || v == 4 || v == 8 || v == 7}
+    val sum = accum._2 + decoded.mkString("").toInt
+    (count, sum)
   }
 
-  println(s"Min: $min")
+  println(s"Part 1: (1, 4, 7, 8): ${count._1}")
+  println(s"Part 2: (Sum): ${count._2}")
 }
