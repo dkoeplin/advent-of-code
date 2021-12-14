9fbb4ca0b520b227b2484d5d793bed1e87349680
David Alan Koeplinger <david.koeplinger@sambanovasystems.com>
2021-12-14 15:13:01 -0500

    Commit!

--- src/Main.scala
+++ src/Main.scala
@@ -1,61 +1,43 @@
 import scala.collection.mutable
 
+case class Point(x: Int, y: Int)
+case class Fold(x: Boolean, coord: Int)
+
 object Main extends App {
-  val NodePattern = "([a-zA-Z]+)-([a-zA-Z]+)".r
-  val file = scala.io.Source.fromFile("./data/12")
-  val edges = file.getLines().map{case NodePattern(a, b) => (a,b) }.toArray
-  val nodes: Array[String] = edges.flatMap{case (a,b) => Seq(a, b) }.distinct
-  val small: Set[Int] = nodes.iterator.zipWithIndex.filter(_._1.head.isLower).map(_._2).toSet
-  val names: Map[String, Int] = nodes.zipWithIndex.toMap
-  val neighbors = mutable.Map.empty[Int,Set[Int]]
-
-  object Node { def unapply(x: String): Option[Int] = Some(names(x)) }
-
-  edges.foreach{case (Node(a), Node(b)) =>
-    neighbors(a) = neighbors.getOrElse(a, Set.empty) + b
-    neighbors(b) = neighbors.getOrElse(b, Set.empty) + a
-  }
-  val start = names("start")
-  val end = names("end")
-
-  def dot(): Unit = {
-    val out = new java.io.PrintWriter(new java.io.File("out/12.gv"))
-    out.write("graph graphname {\n")
-    nodes.foreach { case name@Node(n) =>
-      out.write(s"  $name [label=$name, color=${if (small.contains(n)) "red" else "green"}];\n")
-    }
-    edges.foreach { case (a, b) =>
-      out.write(s"  $a -- $b;\n")
-    }
-    out.write("}\n")
-    out.close()
+  val PointPattern = "([0-9]+),([0-9]+)".r
+  val FoldPattern = "fold along ([xy])=([0-9]+)".r
+  val file = scala.io.Source.fromFile("./data/13")
+  val points: mutable.Set[Point] = mutable.Set.empty[Point]
+  val folds: mutable.Buffer[Fold] = mutable.Buffer.empty[Fold]
+  file.getLines().foreach {
+    case PointPattern(x, y) => points += Point(x.toInt, y.toInt)
+    case FoldPattern(x, c) => folds += Fold(x == "x", c.toInt)
+    case _ =>
   }
 
-  def dfs(part2: Boolean): Seq[Seq[Int]] = {
-    val paths= mutable.Buffer.empty[Seq[Int]]
-    val frontier = mutable.Buffer[(Int,Seq[Int])]((start,Nil))
-    while (frontier.nonEmpty) {
-      val (current, path) = frontier.last
-      frontier.remove(frontier.size - 1)
-      if (current == end) {
-        paths += (path :+ end)
-      } else if (path.count(_ == current) < 100) { // Just in case infinite loops are possible
-        val counts = path.filter(small.contains).groupBy(x=>x).mapValues(_.length)
-        val small2 = counts.exists(_._2 >= 2)
-        val blocked = if (part2 && !small2) Set(start) else path.toSet intersect small
-        val next = neighbors(current) diff blocked
-        //println(s"$current path: [${path.mkString("->")}] next: ${next.mkString(", ")}")
-        if (!small.contains(current) || !small2 || counts.getOrElse(current, 0) < 1)
-          frontier ++= next.map{i => (i,path :+ current) }
-      }
+  def fold(pt: Int, axis: Int): Int = if (pt < axis) pt else 2*axis - pt
+  def makeFolds(folds: Iterator[Fold]): Set[Point] = folds.foldLeft(points.toSet){case (pts, Fold(xAxis, coord)) =>
+    pts.map{
+      case Point(x, y) if xAxis => Point(fold(x, coord), y)
+      case Point(x, y) if !xAxis => Point(x, fold(y, coord))
     }
-    paths
   }
 
-  dot()
-  val part1 = dfs(false)
-  val part2 = dfs(true)
-  part2.foreach{path => println(path.map{nodes.apply}.mkString(",")) }
+  val part1 = makeFolds(folds.iterator.take(1))
+  val part2 = makeFolds(folds.iterator)
+
+  def visualize(points: Set[Point]): Unit = {
+    val xMax = points.map(_.x).max
+    val yMax = points.map(_.y).max
+    (0 to yMax).foreach{y =>
+      val line = (0 to xMax).map{x =>
+        if (points.contains(Point(x, y))) '#' else ' '
+      }.mkString("")
+      println(line)
+    }
+  }
+
   println(s"Part 1: ${part1.size}")
   println(s"Part 2: ${part2.size}")
+  visualize(part2) // Cheating
 }
