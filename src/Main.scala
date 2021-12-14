3a4e8065407527a00d8c219039448f11d449c3c5
David Alan Koeplinger <david.koeplinger@sambanovasystems.com>
2021-12-14 12:22:44 -0500

    Commit!

--- src/Main.scala
+++ src/Main.scala
@@ -1,51 +1,61 @@
 import scala.collection.mutable
 
-class Matrix[T](vs: Iterator[Iterable[T]]) {
-  val data: Array[mutable.Seq[T]] = vs.map{vs => mutable.Seq.empty[T] ++ vs }.toArray
-  val rows: Int = data.length
-  val cols: Int = if (data.nonEmpty) data(0).length else 0
-  def get(i: Int, j: Int): Option[T] = if (i >= 0 && i < rows && j >= 0 && j < cols) { Some(data(i)(j)) } else None
-  def apply(i: Int, j: Int): T = data(i)(j)
-  def update(i: Int, j: Int, value: T): Unit = if (i >= 0 && i < rows && j >= 0 && j < cols) { data(i)(j) = value }
-  def update(i: Int, j: Int, value: Option[T]): Unit = value.foreach{v => update(i, j, v) }
-
-  def indices(): Seq[(Int,Int)] = (0 until rows).flatMap{i => (0 until cols).map{j => (i,j) }}
-
-  override def toString: String = data.map{vs => vs.map{x => Matrix.digit(x)}.mkString("")}.mkString("\n")
-}
-object Matrix {
-  def apply[T](vs: Iterator[Iterable[T]]): Matrix[T] = new Matrix(vs)
-  def digit[T](x: T): String = x match {
-    case x: Int => val str = Integer.toHexString(x); if (str.length > 1) "N" else str
-    case _ => x.toString
-  }
-}
-
 object Main extends App {
-  def neighbors() = (-1 to 1).iterator.flatMap{i => (-1 to 1).map{j => (i, j)}}.filterNot{case (i,j) => i == 0 && j == 0}
+  val NodePattern = "([a-zA-Z]+)-([a-zA-Z]+)".r
+  val file = scala.io.Source.fromFile("./data/12")
+  val edges = file.getLines().map{case NodePattern(a, b) => (a,b) }.toArray
+  val nodes: Array[String] = edges.flatMap{case (a,b) => Seq(a, b) }.distinct
+  val small: Set[Int] = nodes.iterator.zipWithIndex.filter(_._1.head.isLower).map(_._2).toSet
+  val names: Map[String, Int] = nodes.zipWithIndex.toMap
+  val neighbors = mutable.Map.empty[Int,Set[Int]]
 
-  val file = scala.io.Source.fromFile("./data/11")
-  val grid = Matrix(file.getLines().map(_.map(_ - '0')))
-  var flashes: Int = 0
-  def increment(i: Int, j: Int): Boolean = {
-    grid(i, j) = grid.get(i,j).map(_ + 1)
-    grid.get(i,j).contains(10)
+  object Node { def unapply(x: String): Option[Int] = Some(names(x)) }
+
+  edges.foreach{case (Node(a), Node(b)) =>
+    neighbors(a) = neighbors.getOrElse(a, Set.empty) + b
+    neighbors(b) = neighbors.getOrElse(b, Set.empty) + a
   }
-  def flash(i: Int, j: Int): Iterable[(Int,Int)] = {
-    flashes += 1
-    neighbors().map{case (di, dj) => (i + di, j + dj)}.filter{case (i, j) => increment(i, j) }.toSeq
-  }
-  def step(): Unit = {
-    var flashers: Seq[(Int,Int)] = grid.indices().filter{case (i,j) => increment(i,j) }
-    while (flashers.nonEmpty) {
-      flashers = flashers.flatMap{case (i,j) => flash(i,j) }
+  val start = names("start")
+  val end = names("end")
+
+  def dot(): Unit = {
+    val out = new java.io.PrintWriter(new java.io.File("out/12.gv"))
+    out.write("graph graphname {\n")
+    nodes.foreach { case name@Node(n) =>
+      out.write(s"  $name [label=$name, color=${if (small.contains(n)) "red" else "green"}];\n")
     }
-    grid.indices().foreach{case (i,j) => grid(i, j) = grid.get(i, j).filter(_ >= 10).map(_ => 0) }
+    edges.foreach { case (a, b) =>
+      out.write(s"  $a -- $b;\n")
+    }
+    out.write("}\n")
+    out.close()
   }
 
-  (1 to 100).foreach{_ => step() }
-  println(s"Part 1: Flashes: $flashes")
+  def dfs(part2: Boolean): Seq[Seq[Int]] = {
+    val paths= mutable.Buffer.empty[Seq[Int]]
+    val frontier = mutable.Buffer[(Int,Seq[Int])]((start,Nil))
+    while (frontier.nonEmpty) {
+      val (current, path) = frontier.last
+      frontier.remove(frontier.size - 1)
+      if (current == end) {
+        paths += (path :+ end)
+      } else if (path.count(_ == current) < 100) { // Just in case infinite loops are possible
+        val counts = path.filter(small.contains).groupBy(x=>x).mapValues(_.length)
+        val small2 = counts.exists(_._2 >= 2)
+        val blocked = if (part2 && !small2) Set(start) else path.toSet intersect small
+        val next = neighbors(current) diff blocked
+        //println(s"$current path: [${path.mkString("->")}] next: ${next.mkString(", ")}")
+        if (!small.contains(current) || !small2 || counts.getOrElse(current, 0) < 1)
+          frontier ++= next.map{i => (i,path :+ current) }
+      }
+    }
+    paths
+  }
 
-  val part2 = (101 to 1000).find{_ => step(); grid.indices().forall{case (i,j) => grid(i,j) == 0 }}
-  println(s"Part 2: Step: ${part2.map(_.toString).getOrElse("> 1000")}")
+  dot()
+  val part1 = dfs(false)
+  val part2 = dfs(true)
+  part2.foreach{path => println(path.map{nodes.apply}.mkString(",")) }
+  println(s"Part 1: ${part1.size}")
+  println(s"Part 2: ${part2.size}")
 }
