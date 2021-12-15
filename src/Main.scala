55a128469751a17e16b972647220be9ab4013a26
David Alan Koeplinger <david.koeplinger@sambanovasystems.com>
2021-12-14 21:20:09 -0500

    Commit!

--- src/Main.scala
+++ src/Main.scala
@@ -1,43 +1,50 @@
 import scala.collection.mutable
 
-case class Point(x: Int, y: Int)
-case class Fold(x: Boolean, coord: Int)
+case class Pair(a: Char, b: Char)
+object Pair {
+  def apply(x: String): Pair = Pair(x.charAt(0),x.charAt(1))
+}
 
 object Main extends App {
-  val PointPattern = "([0-9]+),([0-9]+)".r
-  val FoldPattern = "fold along ([xy])=([0-9]+)".r
-  val file = scala.io.Source.fromFile("./data/13")
-  val points: mutable.Set[Point] = mutable.Set.empty[Point]
-  val folds: mutable.Buffer[Fold] = mutable.Buffer.empty[Fold]
-  file.getLines().foreach {
-    case PointPattern(x, y) => points += Point(x.toInt, y.toInt)
-    case FoldPattern(x, c) => folds += Fold(x == "x", c.toInt)
-    case _ =>
+  val Rule = "([A-Z])([A-Z]) -> ([A-Z])".r
+  val file = scala.io.Source.fromFile("./data/14")
+  val lines = file.getLines()
+  val init: String = lines.next()
+  val rules: Map[Pair,Char] = lines.flatMap{case Rule(a,b,c) => Some(Pair(a.head,b.head) -> c.head); case _ => None }.toMap
+
+  val pairs = mutable.Map.empty[Pair,Long]
+  init.sliding(2,1).foreach{x =>
+    val pair = Pair(x)
+    pairs(pair) = pairs.getOrElse(pair, 0L) + 1L
   }
 
-  def fold(pt: Int, axis: Int): Int = if (pt < axis) pt else 2*axis - pt
-  def makeFolds(folds: Iterator[Fold]): Set[Point] = folds.foldLeft(points.toSet){case (pts, Fold(xAxis, coord)) =>
-    pts.map{
-      case Point(x, y) if xAxis => Point(fold(x, coord), y)
-      case Point(x, y) if !xAxis => Point(x, fold(y, coord))
+  def applyRules(current: mutable.Map[Pair,Long]): mutable.Map[Pair,Long] = {
+    val next = mutable.Map.empty[Pair,Long]
+    current.foreach{
+      case (pair,n) if rules.contains(pair) =>
+        val c = rules(pair)
+        val left = Pair(pair.a, c)
+        val right = Pair(c, pair.b)
+        next(left) = next.getOrElse(left, 0L) + n
+        next(right) = next.getOrElse(right, 0L) + n
+      case (pair,n) => next(pair) = n
     }
+    next
+  }
+  case class Part(pairs: mutable.Map[Pair,Long], mce: (Char,Long), lce: (Char,Long), diff: Long)
+  def part(offset: Int, steps: Int, initial: mutable.Map[Pair,Long]): Part = {
+    val result = (offset until offset + steps).foldLeft(initial){case (current, step) => applyRules(current) }
+    val counts = mutable.Map.empty[Char,Long]
+    result.foreach{case (Pair(a,_), n) => counts(a) = counts.getOrElse(a, 0L) + n }
+    counts(init.last) = counts.getOrElse(init.last, 0L) + 1L
+    val mce = counts.maxBy(_._2)
+    val lce = counts.minBy(_._2)
+    Part(result, mce, lce, mce._2 - lce._2)
   }
 
-  val part1 = makeFolds(folds.iterator.take(1))
-  val part2 = makeFolds(folds.iterator)
+  val part1 = part(1, 10, pairs)
+  val part2 = part(10, 30, part1.pairs)
 
-  def visualize(points: Set[Point]): Unit = {
-    val xMax = points.map(_.x).max
-    val yMax = points.map(_.y).max
-    (0 to yMax).foreach{y =>
-      val line = (0 to xMax).map{x =>
-        if (points.contains(Point(x, y))) '#' else ' '
-      }.mkString("")
-      println(line)
-    }
-  }
-
-  println(s"Part 1: ${part1.size}")
-  println(s"Part 2: ${part2.size}")
-  visualize(part2) // Cheating
+  println(s"Part 1: ${part1.mce}, ${part1.lce} ${part1.diff}")
+  println(s"Part 2: ${part2.mce}, ${part2.lce} ${part2.diff}")
 }
