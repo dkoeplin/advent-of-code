e7c35a077b75712f32b6df04c8e6c8a0999696ae
David Alan Koeplinger <david.koeplinger@sambanovasystems.com>
2021-12-16 03:09:41 -0500

    Part1 working

--- src/Main.scala
+++ src/Main.scala
@@ -21,57 +21,42 @@ object Main extends App {
   case class Point(i: Int, j: Int) {
     def +(rhs: Point): Point = Point(i + rhs.i, j + rhs.j)
   }
+  case class Pair(point: Point, cost: Int)
 
-  def search(rows: Int, cols: Int): Map[Point,Int] = {
+  def search(rows: Int, cols: Int): Int = {
     val start = Point(0, 0)
     val end = Point(rows - 1, cols - 1)
 
     def isValid(point: Point): Boolean = point.i >= 0 && point.i < rows && point.j >= 0 && point.j < cols
 
-    val unvisited = mutable.Set.empty[Point]
-    unvisited ++= (0 until rows).flatMap{i => (0 until cols).map{j => Point(i,j) }}
-    val costs = mutable.Map.empty[Point, Int]
+    val costs = mutable.Map.empty[Point,Int]
+    val visited = mutable.Set.empty[Point]
     def cost(x: Point): Int = costs.getOrElse(x, Int.MaxValue)
-    def next(): Point = {
-      val next = unvisited.minBy(cost)
-      unvisited.remove(next)
-      next
-    }
-    var continue = true
+    implicit def pointOrdering: Ordering[Pair] = (a: Pair, b: Pair) => implicitly[Ordering[Int]].compare(-a.cost, -b.cost)
+
+    val frontier = mutable.PriorityQueue.empty[Pair]
     costs(start) = 0
-    while (continue && unvisited.nonEmpty) {
-      val current = next()
-      continue = current != end
-      neighbors.iterator.map(_ + current).filter(isValid).filter(unvisited.contains).foreach{n =>
-        costs(n) = Math.min(cost(n), costs(current) + risk(n))
+    frontier.enqueue(Pair(start, 0))
+
+    var continue = true
+    while (continue && frontier.nonEmpty) {
+      val current = frontier.dequeue()
+      continue = current.point != end
+
+      if (continue && !visited.contains(current.point)) {
+        visited += current.point
+        neighbors.iterator.map(_ + current.point).filter(isValid).filterNot(visited.contains).foreach{n =>
+          val newCost = cost(current.point) + risk(n)
+          if (newCost < cost(n)) {
+            costs(n) = newCost
+            frontier.enqueue(Pair(n, newCost))
+          }
+        }
       }
     }
-    costs.toMap
+    costs(end)
   }
 
-  def show(rows: Int, cols: Int): Unit = {
-    (0 until rows).foreach{i =>
-      println((0 until cols).map{j => risk(Point(i,j)) }.mkString(""))
-    }
-  }
-
-  def show(rows: Int, cols: Int, costs: Map[Point,Int]): Unit = {
-    val head = (0 until cols).map{i => val l = i.toString; s"${" "*(3 - l.length)}$l" }.mkString("|")
-    println(s"   ||$head")
-    println("-" * (head.length+6))
-    (0 until rows).foreach { i =>
-      val line = (0 until cols).iterator.map { j => costs.getOrElse(Point(i, j), 999).toString }.map { x => s"${" " * Math.max(3 - x.length, 0)}$x" }.mkString("|")
-      val n = i.toString
-      println(s"${" "*(3 - n.length)}$n||$line")
-      println("-" * (line.length+6))
-    }
-  }
-
-  val part1Costs = search(tileRows, tileCols)
-  val part1 = part1Costs(Point(tileRows - 1, tileCols - 1))
-  println(s"Part 1: $part1")
-
-  val part2Costs = search(tileRows*5, tileCols*5)
-  val part2 = part2Costs(Point(tileRows*5 - 1, tileCols*5 - 1))
-  println(s"Part 2: $part2")
+  println(s"Part 1: ${search(tileRows, tileCols)}")
+  println(s"Part 2: ${search(tileRows*5, tileCols*5)}")
 }
