a9c9fae3b821c05226ee9e27839aae6a6eec87f5
David Alan Koeplinger <david.koeplinger@sambanovasystems.com>
2021-12-17 01:48:07 -0500

    cleanup

--- src/Main.scala
+++ src/Main.scala
@@ -1,62 +1,122 @@
 import scala.collection.mutable
 
-object Main extends App {
-  val Rule = "([A-Z])([A-Z]) -> ([A-Z])".r
-  val file = scala.io.Source.fromFile("./data/15")
-  val grid: Array[Array[Int]] = file.getLines().map(_.map(_ - '0').toArray).toArray
-  val tileRows = grid.length
-  val tileCols = grid(0).length
+case class Header(version: Long, tp: Long)
 
-  val neighbors = Seq(Point(-1, 0), Point(1, 0), Point(0, -1), Point(0, 1))
-
-  def risk(point: Point): Int = {
-    val i = point.i % tileRows
-    val j = point.j % tileCols
-    val tile_i = point.i / tileRows
-    val tile_j = point.j / tileCols
-    val base = grid(i)(j) + tile_i + tile_j
-    if (base > 9) base % 9 else base
+abstract class Packet(val header: Header) {
+  val id: Int = { Packet.id += 1; Packet.id }
+  final override def toString: String = s"%$id"
+  final override def hashCode(): Int = id
+  def readable: String
+  def evaluate: Long
+}
+case class Literal(override val header: Header, literal: Long) extends Packet(header) {
+  def readable: String = s"%$id = ${header.version}.Const($literal)"
+  def evaluate: Long = literal
+}
+case class Operator(override val header: Header, packets: Seq[Packet], end: Int) extends Packet(header) {
+  def readable: String = s"%$id = ${header.version}.${Packet.opNames(header.tp)}(${packets.mkString(", ")})"
+  def evaluate: Long = {
+    val elems = packets.map(_.evaluate)
+    header.tp match {
+      case 0 => elems.sum
+      case 1 => elems.product
+      case 2 => elems.min
+      case 3 => elems.max
+      case 5 => if (elems(0) > elems(1)) 1L else 0L
+      case 6 => if (elems(0) < elems(1)) 1L else 0L
+      case 7 => if (elems(0) == elems(1)) 1L else 0L
+      case tp => sys.error(s"Invalid packet type: $tp")
+    }
   }
+}
 
-  case class Point(i: Int, j: Int) {
-    def +(rhs: Point): Point = Point(i + rhs.i, j + rhs.j)
-  }
-  case class Pair(point: Point, cost: Int)
+case class PacketBuilder(
+  header: Header,
+  byPacketCount: Boolean, // 0 - total length of subpackets (15b), 1 - # subpackets (11)
+  end: Long,
+  packets: mutable.Buffer[Packet] = mutable.Buffer.empty[Packet]
+) {
+  def reify(pos: Int): Operator = Operator(header, packets, pos)
+}
 
-  def search(rows: Int, cols: Int): Int = {
-    val start = Point(0, 0)
-    val end = Point(rows - 1, cols - 1)
+object Packet {
+  type Bits = String
+  var id: Int = -1
 
-    def isValid(point: Point): Boolean = point.i >= 0 && point.i < rows && point.j >= 0 && point.j < cols
+  val opNames: Map[Long,String] = Map[Long,String](
+    0L -> "Sum", 1L -> "Product", 2L -> "Min", 3L -> "Max",
+    5L -> "GreaterThan",  6L -> "LessThan", 7L -> "Equals"
+  )
 
-    val costs = mutable.Map.empty[Point,Int]
-    val visited = mutable.Set.empty[Point]
-    def cost(x: Point): Int = costs.getOrElse(x, Int.MaxValue)
-    implicit def pointOrdering: Ordering[Pair] = (a: Pair, b: Pair) => implicitly[Ordering[Int]].compare(-a.cost, -b.cost)
+  val hex: Map[Char,Bits] = Map[Char,Bits](
+    '0' -> "0000", '1' -> "0001", '2' -> "0010", '3' -> "0011",
+    '4' -> "0100", '5' -> "0101", '6' -> "0110", '7' -> "0111",
+    '8' -> "1000", '9' -> "1001", 'A' -> "1010", 'B' -> "1011",
+    'C' -> "1100", 'D' -> "1101", 'E' -> "1110", 'F' -> "1111")
 
-    val frontier = mutable.PriorityQueue.empty[Pair]
-    costs(start) = 0
-    frontier.enqueue(Pair(start, 0))
+  def parse(x: Bits): Seq[Packet] = {
+    var pos: Int = 0
+    def parseBool(): Boolean = { val value = x.charAt(pos) == '1'; pos += 1; value }
+    def rawBits(n: Int): Bits = { val value = x.substring(pos, pos + n); pos += n; value }
+    def parseInt(n: Int): Long = BigInt(rawBits(n), 2).longValue()
 
-    var continue = true
-    while (continue && frontier.nonEmpty) {
-      val current = frontier.dequeue()
-      continue = current.point != end
+    def done(builder: PacketBuilder): Boolean = {
+      if (builder.byPacketCount) builder.packets.size >= builder.end else pos >= builder.end
+    }
 
-      if (continue && !visited.contains(current.point)) {
-        visited += current.point
-        neighbors.iterator.map(_ + current.point).filter(isValid).filterNot(visited.contains).foreach{n =>
-          val newCost = cost(current.point) + risk(n)
-          if (newCost < cost(n)) {
-            costs(n) = newCost
-            frontier.enqueue(Pair(n, newCost))
-          }
+    val packets = mutable.Buffer.empty[Packet]
+    val stack = mutable.Stack[PacketBuilder]()
+
+    def finishPacket(packet: Packet): Unit = {
+      packets += packet
+      if (stack.nonEmpty)
+        stack.top.packets += packet
+    }
+
+    do {
+      // Header
+      // (version: 3b)(type: 3b)
+
+      // Type 4 - Literal
+      // [Header: 6b]1(4b)1(4b) ... 0(4b) (0 padding to 4 bits)
+
+      // Operators
+      // [Header: 6b]0(length of subpackets in bits: 15b)(subpackets)
+      // [Header: 6b]1(number of subpackets: 11b)(subpackets)
+
+      if (stack.nonEmpty && done(stack.top)) {
+        val current = stack.pop()
+        finishPacket(current.reify(pos))
+      } else {
+        val header = Header(version = parseInt(3), tp = parseInt(3))
+        if (header.tp == 4) {
+          var continue = true
+          val bits = new mutable.StringBuilder
+          while (continue) { continue = parseBool(); bits ++= rawBits(4) }
+          val packet = Literal(header, BigInt(bits.mkString, 2).longValue())
+          finishPacket(packet)
+        } else {
+          val byPacketCount = parseBool()
+          val end = if (byPacketCount) parseInt(11) else parseInt(15) + pos
+          stack.push(PacketBuilder(header, byPacketCount, end))
         }
       }
-    }
-    costs(end)
-  }
+    } while (stack.nonEmpty && pos < x.length)
 
-  println(s"Part 1: ${search(tileRows, tileCols)}")
-  println(s"Part 2: ${search(tileRows*5, tileCols*5)}")
+    packets
+  }
+}
+
+object Main extends App {
+  val file = scala.io.Source.fromFile("./data/16")
+  file.getLines().zipWithIndex.filterNot(_._1.startsWith("#")).foreach{case (line, idx) =>
+    println(s"Line ${idx+1}: $line:")
+    val binary = line.flatMap{c => Packet.hex(c) }.mkString
+    val packets = Packet.parse(binary)
+    packets.foreach{p => println(s"  ${p.readable}") }
+    val part1 = packets.foldLeft(0L){case (accum, packet) => accum + packet.header.version }
+    val part2 = packets.last.evaluate
+    println(s"  Part 1 (Version Sum): $part1")
+    println(s"  Part 2 (Evaluation): $part2")
+  }
 }
