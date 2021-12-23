bf58ea2b3a8a2c0852ea79781524a91e83549a8e
David Alan Koeplinger <david.koeplinger@sambanovasystems.com>
2021-12-23 14:30:27 -0500

    Day 20

---
+++ src/Day20.scala
@@ -0,0 +1,31 @@
+import common.Matrix
+
+object Day20 extends App {
+  val window = Seq((-1,-1),(-1,0),(-1,1),(0,-1),(0,0),(0,1),(1,-1),(1,0),(1,1))
+
+  val file = io.Source.fromFile("./data/20")
+  val lines = file.getLines()
+  val table: Array[Int] = lines.next().map{case '#' => 1 case '.' => 0}.toArray
+  val data = lines.filter(_.nonEmpty).map{line => line.map{case '#' => 1 case '.' => 0}}
+  val input: Matrix[Int] = Matrix(data)
+
+  def enhance(x: Matrix[Int], default: Int): Matrix[Int] = Matrix((-1 to x.rows).iterator.map{i =>
+    (-1 to x.cols).map { j =>
+      val w = window.map{case (di, dj) => x.getOrElse(i + di, j + dj, default) }.mkString("")
+      table(Integer.parseInt(w, 2))
+    }
+  })
+
+  def enhanceN(x: Matrix[Int], steps: Int): Matrix[Int] = {
+    (0 until steps).foldLeft(x){case (mat, i) =>
+      val default = if (table(0) == 1) i % 2 else 0
+      enhance(mat, default)
+    }
+  }
+
+  val part1 = enhanceN(input, 2)
+  println(s"Part 1 (Total): ${part1.sum}")
+
+  val part2 = enhanceN(input, 50)
+  println(s"Part 2 (Total): ${part2.sum}")
+}
--- src/common/Matrix.scala
+++ src/common/Matrix.scala
@@ -7,11 +7,20 @@ class Matrix[T](vs: Iterator[Iterable[T]]) {
   val rows: Int = data.length
   val cols: Int = if (data.nonEmpty) data(0).length else 0
   def get(i: Int, j: Int): Option[T] = if (i >= 0 && i < rows && j >= 0 && j < cols) { Some(data(i)(j)) } else None
+  def getOrElse(i: Int, j: Int, default: => T): T = get(i, j).getOrElse(default)
   def apply(i: Int, j: Int): T = data(i)(j)
   def update(i: Int, j: Int, value: T): Unit = if (i >= 0 && i < rows && j >= 0 && j < cols) { data(i)(j) = value }
   def update(i: Int, j: Int, value: Option[T]): Unit = value.foreach{v => update(i, j, v) }
 
   def indices(): Seq[(Int,Int)] = (0 until rows).flatMap{i => (0 until cols).map{j => (i,j) }}
+
+  def mapIndices[R](func: (Int,Int) => R): Matrix[R] = Matrix(
+    (0 until rows).iterator.map{i =>
+      (0 until cols).map{j => func(i,j) }
+    }
+  )
+
+  def sum(implicit num: Numeric[T]): T = data.map(_.reduce(num.plus)).reduce(num.plus)
 
   override def toString: String = data.map{vs => vs.map{x => Matrix.digit(x)}.mkString("")}.mkString("\n")
 }
