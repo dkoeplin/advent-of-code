a0e50ac780ca944d25cd420ea871e64c2775b152
David Alan Koeplinger <david.koeplinger@sambanovasystems.com>
2022-12-17 15:01:45 -0500

    start 15

---
+++ src/Year2022/Day15.scala
@@ -0,0 +1,16 @@
+package Year2022
+
+import common.Pos
+import scala.util.matching.Regex
+
+object Day15 extends App {
+  case class Pair(sensor: Pos, beacon: Pos)
+  object Pair {
+    val pattern: Regex = "Sensor at x=([-0-9]+), y=([-0-9]+): closest beacon is at x=([-0-9]+), y=([-0-9]+)".r
+    def parse(line: String): Pair = pattern.findFirstMatchIn(line).map{m =>
+      Pair(Pos(m.group(2).toInt, m.group(1).toInt), Pos(m.group(4).toInt, m.group(3).toInt))
+    }.getOrElse(throw new Exception(s"Unable to parse $line"))
+  }
+
+  val file =
+}
