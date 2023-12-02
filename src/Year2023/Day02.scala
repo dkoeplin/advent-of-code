260f45fa99c19068b2a91ddccc25ce6adcfba7ae
davidk <dkoeplinger@gmail.com>
2023-12-02 10:41:43 -0500

    Day 2

--- src/Year2023/Day02.scala
+++ src/Year2023/Day02.scala
@@ -7,26 +7,20 @@ object Day02 extends Year2023(2) {
     def unapply(x: String): Option[Color.Value] = map.get(x)
   }
   type Color = Color.Value
-  val R = Color.Red
-  val G = Color.Green
-  val B = Color.Blue
-  val Colors = List(R, G, B)
+  val Colors = List(Color.Red, Color.Green, Color.Blue)
 
-  class Round(init: Iterable[(Color, Int)]) {
-    def this(init: (Color, Int)*) = this(init)
+  class Round(val cubes: Map[Color, Int]) {
     def apply(c: Color): Int = cubes.getOrElse(c, 0)
     def <=(rhs: Round): Boolean = Colors.forall{c => this(c) <= rhs(c) }
-    def max(rhs: Round): Round = new Round(Colors.map{c => (c, math.max(this(c), rhs(c))) })
+    def max(rhs: Round): Round = new Round(Colors.map{c => (c, math.max(this(c), rhs(c))) }.toMap)
     def power(): Int = Colors.map{c => this(c) }.product
-
-    private val cubes: Map[Color, Int] = init.toMap
   }
   object Round {
     def unapply(x: String): Option[Round] = Some(new Round(
       x.split(',').iterator
         .map(_.trim.split(' '))
         .collect { case Array(n, Color(c)) => (c, n.toInt) }
-        .toArray.groupBy(_._1).view.mapValues(_.map(_._2).sum)
+        .toArray.groupBy(_._1).view.mapValues(_.map(_._2).sum).toMap
     ))
   }
 
@@ -34,7 +28,7 @@ object Day02 extends Year2023(2) {
     game.split(':').last.split(';').collect{case Round(r) => r}
   }.toArray
 
-  val part1_target = new Round((R, 12), (G, 13), (B, 14))
+  val part1_target = new Round(Map((Color.Red, 12), (Color.Green, 13), (Color.Blue, 14)))
   val part1 = games.iterator.zipWithIndex
                    .filter{game => game._1.forall(_ <= part1_target) }
                    .map(_._2 + 1).sum
