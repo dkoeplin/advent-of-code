e8a4abf15879a7c4d8533d76b43701a02bb6b9d7
davidk <dkoeplinger@gmail.com>
2023-12-04 16:31:56 -0500

    Day 4

---
+++ src/Year2023/Day04.scala
@@ -0,0 +1,27 @@
+package Year2023
+
+object Day04 extends Year2023(4) {
+  case class Card(win: Set[Int], has: Set[Int]) {
+    val matching: Int = has.count(win.contains)
+    val points: Long = if (matching > 0) 1L << (matching - 1) else 0L
+  }
+  val cards = data.getLines().map{line =>
+    val Array(win, has) = line.split(':').last
+                              .split('|').map( _.split(' ').iterator.map(_.trim).filter(_.nonEmpty).map(_.toInt).toSet)
+    Card(win, has)
+  }.toArray
+
+  val part1 = cards.iterator.map(_.points).sum
+  println(s"Part 1: $part1")
+
+  // Just keep an array for the next N copies, dropping the first each time we reduce it
+  val part2 = cards.foldLeft((Array.empty[Int], 0)){case ((repeats, n), card) =>
+    val copies = if (repeats.isEmpty) 1 else repeats.head
+    // Not super elegant, but this is just previously known copies + new copies
+    val repeats2 = Array.tabulate(math.max(repeats.length - 1, card.matching)){j =>
+      (if (repeats.length > j + 1) repeats(j + 1) else 1) + (if (j < card.matching) copies else 0)
+    }
+    (repeats2, n + copies)
+  }
+  println(s"Part 2: ${part2._2}")
+}
