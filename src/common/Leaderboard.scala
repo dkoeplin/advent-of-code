702c9a6b46ece6cc65a1754a66fd1d759ef90c61
davidk <dkoeplinger@gmail.com>
2023-12-16 18:31:53 -0500

    Leaderboard

---
+++ src/common/Leaderboard.scala
@@ -0,0 +1,38 @@
+package common
+
+import java.net.URI
+import java.util.Scanner
+import scala.util.matching.Regex
+
+case class Page(url: String) {
+  val contents: String = {
+    val sc = new Scanner(new URI(url).toURL.openStream)
+    val sb = new StringBuffer
+    while (sc.hasNext) sb.append(sc.next)
+    sb.toString
+  }
+}
+
+case class Records(times: Array[Double]) {
+  lazy val min: Double = times.min
+  lazy val max: Double = times.max
+  lazy val avg: Double = times.sum / times.length
+  lazy val stddev: Double = math.sqrt(times.iterator.map{t => math.pow(avg - t, 2) }.sum/times.length)
+}
+object Records {
+  private val regex = "([0-9]{2}):([0-9]{2}):([0-9]{2})".r
+  private def parse(m: Regex.Match): Double = m.group(1).toDouble * 60 + m.group(2).toDouble + m.group(3).toDouble/60
+  def parse(str: String): Records = {
+    val data = regex.findAllMatchIn(str).map(parse).toArray
+    Records(data.take(data.length / 2))
+  }
+}
+
+object Leaderboard extends App {
+  val site = "https://adventofcode.com/2023/leaderboard/day"
+  (1 to 15).iterator.foreach{day =>
+    val page = Page(s"$site/$day").contents
+    val records = Records.parse(s"$page/$day")
+    println(s"$day, ${records.min}, ${records.avg}, ${records.max}")
+  }
+}
